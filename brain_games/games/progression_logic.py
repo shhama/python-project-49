ae6debc27392ed16f8c2d68fff0a7def134ba5e1
shhama <shamshetdinov.ilnur@mail.ru>
2023-03-19 14:53:22 +0300

    add progression_logic

---
+++ brain_games/games/progression_logic.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import random
+import prompt
+import string
+
+
+def arithmet_progr():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f'Hello, {name}!')
+    print(f'What number is missing in the progression?')
+    count = 0
+    while count < 3:
+        start = random.randint(1, 100)
+        step = random.randint(1, 5)
+        size = 10
+        a = []
+        for i in range(size):
+            start += step
+            i += start
+            a.append(start)
+        rand_int = random.randint(1, 9)
+        string = [str(i) for i in a]
+        string[rand_int] = ".."
+        join_strng = " ".join(string)
+        print(f'''Question: {join_strng}''')
+        your_answ = prompt.string("Your answer: ")
+        if int(your_answ) != a[rand_int]:
+            print(f''''{your_answ}' is wrong answer ;(. Correct answer was '{a[rand_int]}'.\nLet's try again, {name}!"''')
+            break
+        else:
+            print("Correct!")
+        count += 1
