a9c89bdc7435700f1677e7ca44ed9f010b9da62c
shhama <shamshetdinov.ilnur@mail.ru>
2023-03-20 18:57:35 +0300

    fix progression_logic

--- brain_games/games/progression_logic.py
+++ brain_games/games/progression_logic.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python3
-
 import random
 import prompt
-import string
 
 
 def arithmet_progr():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
-    print(f'What number is missing in the progression?')
+    print('What number is missing in the progression?')
     count = 0
     while count < 3:
         start = random.randint(1, 100)
@@ -27,10 +24,11 @@ def arithmet_progr():
         print(f'''Question: {join_strng}''')
         your_answ = prompt.string("Your answer: ")
         if int(your_answ) != a[rand_int]:
-            print(f''''{your_answ}' is wrong answer ;(. Correct answer was '{a[rand_int]}'.\nLet's try again, {name}!"''')
+            print(f''''{your_answ}' is wrong answer ;(. Correct answer was'''
+                  f''' {a[rand_int]}'.\nLet's try again, {name}!"''')
             break
         else:
             print("Correct!")
-       	    count += 1
+            count += 1
             if count == 3:
                 print(f'''Congratulations, {name}!''')
