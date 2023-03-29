1eab4076a5d3e8ff3077414abdf32f1b51c8aa2a
shhama <shamshetdinov.ilnur@mail.ru>
2023-03-29 17:49:05 +0300

    fix prime_logic

--- brain_games/games/prime_logic.py
+++ brain_games/games/prime_logic.py
@@ -5,22 +5,27 @@ import prompt
 dict = {True: "yes", False: "no"}
 
 
-def is_prime():
-    dict = {True: "yes", False: "no"}
+def is_prime(x):
+    simple = False
+    i = 2
+    for i in range(i, x):
+        if x % i == 0:
+            simple = True
+    return simple
+
+
+def prime_game():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     count = 0
     while count < 3:
-        simple = True
         n = random.randint(1, 100)
+        simple = False
         print(f'''Question: {n}''')
         your_answ = prompt.string("Your answer: ")
-        i = 2
-        for i in range(i, n):
-            if n % i == 0:
-                simple = False
+        simple = is_prime(n)
         correct_answ = dict[simple]
         if your_answ != correct_answ:
             print(f'''"{your_answ}" is wrong answer ;(. Correct answer was'''
