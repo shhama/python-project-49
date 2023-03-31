686586af9bedca1c8adc130f3bb4fbec797eaec6
shhama <shamshetdinov.ilnur@mail.ru>
2023-03-31 13:02:14 +0300

    fix calc_logic

--- brain_games/games/calc_logic.py
+++ brain_games/games/calc_logic.py
@@ -2,7 +2,7 @@ import prompt
 import random
 
 
-def calc_games():
+def play_calc():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
