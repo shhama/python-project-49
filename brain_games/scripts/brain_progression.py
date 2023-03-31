afbad02906ce5ca3d8bf06584d1433f43cf34907
shhama <shamshetdinov.ilnur@mail.ru>
2023-03-31 13:05:37 +0300

    fix brain_progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.games.progression_logic import arithmet_progr
+from brain_games.games.progression_logic import play_progr
 
 
 def main():
-    arithmet_progr()
+    play_progr()
 
 
 if __name__ == '__main__':
