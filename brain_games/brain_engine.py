5ecbee5fab7bf1203192c145a3fc9e387c7f963e
shhama <shamshetdinov.ilnur@mail.ru>
2023-04-03 21:50:55 +0300

    create brain_engine

---
+++ brain_games/brain_engine.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import prompt
+
+
+ATTEMPTS = 3
+
+
+def run(game):
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May i have your name? ")
+    print(f'''Hello, {name}!''')
+    print(game.MESSAGE)
+    tries = ATTEMPTS
+    while tries:
+        question, correct = game.take_game()
+        print(question)
+        answer = prompt.string("Your answer: ")
+        if answer != correct:
+            print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct}".')
+            print(f'''Let's try again, {name}!''')
+            return
+        else:
+            print("Correct!")
+            tries -= 1
+    print(f'''Congratulations, {name}!''')
