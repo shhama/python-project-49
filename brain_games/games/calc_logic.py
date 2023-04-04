fd62710029a81c1e995f5885643ba6a32bd83f9a
shhama <shamshetdinov.ilnur@mail.ru>
2023-04-04 17:27:35 +0300

    fix calc_logic

--- brain_games/games/calc_logic.py
+++ brain_games/games/calc_logic.py
@@ -1,37 +1,23 @@
-import prompt
 import random
+
+
+MESSAGE = 'What is the result of the expression?'
 
 
 FIRST_NUM = 1
 LAST_NUM = 10
+OPERATORS = ["+", "-", "*"]
 
 
-def play_calc():
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f'Hello, {name}!')
-    print('What is the result of the expression?')
-    count = 0
-    while count < 3:
-        operand_first = random.randint(FIRST_NUM, LAST_NUM)
-        operand_second = random.randint(FIRST_NUM, LAST_NUM)
-        operators = ["+", "-", "*"]
-        rand_op = random.choice(operators)
-        expression = f'{operand_first} {rand_op} {operand_second}'
-        dict = {
-            "+": operand_first + operand_second,
-            "-": operand_first - operand_second,
-            "*": operand_first * operand_second
-        }
-        correct_answ = dict[rand_op]
-        print(f'Question: {expression}')
-        your_answ = prompt.string("Your answer: ")
-        if int(your_answ) == (correct_answ):
-            print("Correct!")
-            count += 1
-            if count == 3:
-                print(f'Congratulations, {name}!')
-        else:
-            print(f'''"{your_answ}" is wrong answer ;(. Correct answer was'''
-                  f'''"{correct_answ}".\nLet's try again, {name}!''')
-            break
+def take_game():
+    operand_first = random.randint(FIRST_NUM, LAST_NUM)
+    operand_second = random.randint(FIRST_NUM, LAST_NUM)
+    rand_op = random.choice(OPERATORS)
+    actions = {
+        "+": operand_first + operand_second,
+        "-": operand_first - operand_second,
+        "*": operand_first * operand_second
+    }
+    question = f'{operand_first} {rand_op} {operand_second}'
+    correct = str(actions[rand_op])
+    return question, correct
