0ea2a4fe5d4a318429114f89bc889a4d12c6e55b
shhama <shamshetdinov.ilnur@mail.ru>
2023-04-07 21:16:39 +0300

    fix gcd_logic

--- brain_games/games/gcd_logic.py
+++ brain_games/games/gcd_logic.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
-import prompt
 import random
+
+
+FIRST_NUM = 0
+LAST_NUM = 10
+MESSAGE = "Find the greatest common divisor of given numbers."
 
 
 def is_nod(a, b):
     if a == 0 or b == 0:
-        return a + b
+        return str(a + b)
     if a > b:
-        return is_nod(a - b, b)
+        return str(is_nod(a - b, b))
     else:
-        return is_nod(a, b - a)
+        return str(is_nod(a, b - a))
 
 
-def play_gcd():
-    count = 0
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f'Hello, {name}!')
-    print('Find the greatest common divisor of given numbers.')
-    while count < 3:
-        a = random.randint(0, 100)
-        b = random.randint(0, 100)
-        print(f'Question: {a} {b}')
-        your_answ = prompt.string("Your answer: ")
-        if int(your_answ) == is_nod(a, b):
-            print("Correct!")
-            count += 1
-            if count == 3:
-                print(f'Congratulations, {name}!')
-        else:
-            print(f'''{your_answ} is wrong answer ;(. Correct answer'''
-                  f''' was {is_nod(a, b)}. \nLet's try again, {name}!''')
-            break
+def take_game():
+    a = random.randint(FIRST_NUM, LAST_NUM)
+    b = random.randint(FIRST_NUM, LAST_NUM)
+    question = f'''Question: {a} {b}'''
+    correct = is_nod(a, b)
+    return question, correct
