e806221155cf30745d67a94aed6c9918f75dcaf2
shhama <shamshetdinov.ilnur@mail.ru>
2023-04-07 21:17:30 +0300

    fix brain_prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-from brain_games.games.prime_logic import play_prime
+from brain_games.games import prime_logic
+from brain_games.brain_engine import run
 
 
 def main():
-    play_prime()
+    run(prime_logic)
 
 
 if __name__ == '__main__':
     main()
+
